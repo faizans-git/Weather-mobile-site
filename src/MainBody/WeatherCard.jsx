ee536ab1271b4139b9e4fd8e6627a7ee87f195f6
faizans-git <frog24male@gmail.com>
2025-10-07 18:47:03 +0500

    Push all latest weather site updates

--- src/MainBody/WeatherCard.jsx
+++ src/MainBody/WeatherCard.jsx
@@ -1,26 +1,31 @@
-import './WeatherCard.css';
-
-
-export function WeatherCard({ city,weather }) {
-
-  const timeStamp = weather?.current_weather?.time || '7-9-2025';
+import './WeatherCard.css'
+export function WeatherCard({ city, weather }) {
+  const timeStamp = weather?.current_weather?.time || "7-9-2025";
   const date = new Date(timeStamp);
-  const options = { weekday: "long", year: "numeric", month: "short", day: "numeric" };
-  const properDate = date.toLocaleDateString("en-US", options)
-    
-  
-    return (
-        <div className="weather-card">
-            <div className="card-top">
-              <p className="location">{city.name} , {city.country}</p>
-              <p className="date">{properDate}</p>
-            </div>
-            <div className="card-main">
-              <div className="weather-icon">☀️</div>
-              <div className="temperature">
-                {Math.round(weather.current_weather.temperature)}<span className="degree">°</span>
-              </div>
-            </div>
-          </div>
-    )
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  };
+  const properDate = date.toLocaleDateString("en-US", options);
+
+  return (
+    <div className="weather-card">
+      <div className="card-top">
+        <p className="location">
+          {city.name}, {city.country}
+        </p>
+        <p className="date">{properDate}</p>
+      </div>
+
+      <div className="card-main">
+        <div className="weather-icon">☀️</div>
+        <div className="temperature">
+          {Math.round(weather?.current_weather?.temperature ?? 0)}
+          <span className="degree">°</span>
+        </div>
+      </div>
+    </div>
+  );
 }
